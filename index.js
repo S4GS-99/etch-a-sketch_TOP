3099168e41029d32ab320cee1289b52052dd988f
2G2-99 <79936466+2G2-99@users.noreply.github.com>
2025-02-06 14:33:22 +0000

    Grid generated Now the grid is properly generated and also there is some styling to ensure the display is correct

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const BODY = document.querySelector('body');
 const CONTAINER = document.querySelector('#container');
-const grid = generateContainers(16);
+const grid = generateGrid(16);
 
-function generateContainers(numOfContainers) {
-  for (let i = 0; i < numOfContainers; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add(`cell`);
-    cell.setAttribute('cell-number', `${i + 1}`);
-    CONTAINER.appendChild(cell);
+function generateGrid(size) {
+  for (let column = 1; column < size; column++) {
+    const colElement = document.createElement('div');
+    colElement.classList.add(`column`);
+    colElement.setAttribute('col-number', `${column}`);
+
+    for (let cell = 1; cell <= size; cell++) {
+      const cellElement = document.createElement('div');
+      cellElement.classList.add('cell');
+      cellElement.setAttribute('cel-number', `${cell}`);
+      colElement.appendChild(cellElement);
+    }
+
+    CONTAINER.appendChild(colElement);
   }
 
   return;
 }
 
-function listOfContainers(element, attribute) {
-  return;
-}
+console.log(document.querySelectorAll('.column'));
+console.log(document.querySelectorAll('.cell'));
